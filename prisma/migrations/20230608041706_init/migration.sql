168c64ed31e8aee6fac1471d108788055e16e4b9
SengMitnick <sengmitnick@163.com>
2023-06-08 12:17:36 +0800

    feat: 更新数据库

--- prisma/migrations/20230608041706_init/migration.sql
+++ prisma/migrations/20230608041706_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
+    `pwd` VARCHAR(191) NOT NULL,
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_pwd_key`(`pwd`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
