623ce14d72bdb8830c70f64f01de715d9607d561
SengMitnick <sengmitnick@163.com>
2023-06-14 11:12:11 +0800

    feat: storage功能优化

--- apps/web/utils/storage.ts
+++ apps/web/utils/storage.ts
@@ -1,20 +1,27 @@
+const map = new Map<string, string>();
+
 export const storage = {
   setItem(key: string, value: string) {
-    if (typeof window === 'undefined') return;
-    if (window.self !== window.top) return;
-    if (!window.navigator.cookieEnabled) return;
+    if (typeof window === "undefined") return;
+    if (window.self !== window.top || !window.navigator.cookieEnabled) {
+      map.set(key, value);
+      return;
+    }
     return window.localStorage.setItem(key, value);
   },
   getItem(key: string) {
-    if (typeof window === 'undefined') return null;
-    if (window.self !== window.top) return null;
-    if (!window.navigator.cookieEnabled) return null;
+    if (typeof window === "undefined") return null;
+    if (window.self !== window.top || !window.navigator.cookieEnabled) {
+      return map.get(key) || null;
+    }
     return window.localStorage.getItem(key);
   },
   removeItem(key: string) {
-    if (typeof window === 'undefined') return;
-    if (window.self !== window.top) return;
-    if (!window.navigator.cookieEnabled) return;
+    if (typeof window === "undefined") return;
+    if (window.self !== window.top || !window.navigator.cookieEnabled) {
+      map.delete(key);
+      return;
+    }
     return window.localStorage.removeItem(key);
   },
 };
