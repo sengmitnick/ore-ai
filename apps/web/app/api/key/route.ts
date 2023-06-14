9bd9b7cf2b4d6da9dcdae92b831fca84c2ff5145
SengMitnick <sengmitnick@163.com>
2023-06-14 19:54:38 +0800

    fix: Export encountered errors on following paths

--- apps/web/app/api/key/route.ts
+++ apps/web/app/api/key/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(): Promise<Response> {
+export async function GET() {
   return NextResponse.json({ key: process.env.API_KEY });
 }
